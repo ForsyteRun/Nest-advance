de57532fa1c56f5f2ff97ac7e7fe209d8896bcc2
Forsyte <forsyterun@gmail.com>
2025-06-27 19:05:50 +0200

    feat: @Query

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller({
+  path: 'user',
+  host: 'localhost',
+})
 export class UserController {
   constructor(private readonly userService: UserService) { }
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  findAll(@Query() query: string) {
+    return query
   }
 }
