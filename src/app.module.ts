84ef4b8b6b07f8330897984f7a69d7991a2c1396
forsyteru <forsyterun@gmail.com>
2025-07-15 18:10:26 +0200

    feat: PrismaService

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,17 @@ import { ExecutorModule } from './executor/executor.module';
 import { ReviewModule } from './review/review.module';
 import { TaskModule } from './task/task.module';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
     TaskModule,
     UserModule,
     ReviewModule,
     ExecutorModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
