84a49980641d86c22c16c736d98f3746f9a98a36
forsyteru <forsyterun@gmail.com>
2025-07-16 16:28:59 +0200

    fix: add Injectable

--- src/common/middlewares/logger.middleware.ts
+++ src/common/middlewares/logger.middleware.ts
@@ -1,6 +1,7 @@
-import type { NestMiddleware } from '@nestjs/common';
+import { type NestMiddleware, Injectable } from '@nestjs/common';
 import type { NextFunction, Request, Response } from 'express';
 
+@Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     console.log('Request...', req.method, req.url);
