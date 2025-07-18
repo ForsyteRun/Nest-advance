7cd166c16bc177f25632f23b1eac873c37940b27
forsyteru <forsyterun@gmail.com>
2025-07-18 14:27:52 +0200

    feat: Filters

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterseptor } from './common/interseptors/response.interseptor';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { AllExceptionsFilter } from './common/filters/all-exeptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(LoggerMiddleware);
   app.useGlobalInterceptors(new ResponseInterseptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 }
